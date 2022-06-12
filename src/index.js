f9a8951c8967f8efef86c2c0475e21b772bf4b8d
Nurmuhammet Saryyev <nsaryyev9797@gmail.com>
2022-06-12 23:14:29 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="weather-app">
       <StoreProvider>
         <MainRoutes />
       </StoreProvider>
